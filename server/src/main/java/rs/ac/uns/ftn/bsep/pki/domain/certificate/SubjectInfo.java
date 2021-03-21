28313e5cfbf7dd286fd0a4932a3ee817df35be2a
Nikola Pantelic <nikola.pantelic@uns.ac.rs>
2021-03-21 22:57:31 +0100

    Certificate request model

--- server/src/main/java/rs/ac/uns/ftn/bsep/pki/domain/certificate/SubjectInfo.java
+++ server/src/main/java/rs/ac/uns/ftn/bsep/pki/domain/certificate/SubjectInfo.java
@@ -1,9 +1,10 @@
-package rs.ac.uns.ftn.bsep.pki.domain.dto;
+package rs.ac.uns.ftn.bsep.pki.domain.certificate;
 
-import rs.ac.uns.ftn.bsep.pki.domain.certificate.SubjectInfo;
+import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.asn1.x500.X500NameBuilder;
+import org.bouncycastle.asn1.x500.style.BCStyle;
 
-public class SubjectInfoDTO {
-
+public class SubjectInfo {
     private String countryName;
     private String stateName;
     private String localityName;
@@ -14,19 +15,41 @@ public class SubjectInfoDTO {
     private String givenName;
     private String emailAddress;
 
-    public SubjectInfoDTO() {
+    public SubjectInfo(){ }
+
+    public SubjectInfo(String countryName,
+                       String stateName,
+                       String localityName,
+                       String organizationName,
+                       String organizationalUnitName,
+                       String commonName,
+                       String surname,
+                       String givenName,
+                       String emailAddress) {
+        this.countryName = countryName;
+        this.stateName = stateName;
+        this.localityName = localityName;
+        this.organizationName = organizationName;
+        this.organizationalUnitName = organizationalUnitName;
+        this.commonName = commonName;
+        this.surname = surname;
+        this.givenName = givenName;
+        this.emailAddress = emailAddress;
     }
 
-    public SubjectInfo toSubjectInfo() {
-        return new SubjectInfo(countryName,
-                stateName,
-                localityName,
-                organizationName,
-                organizationalUnitName,
-                commonName,
-                surname,
-                givenName,
-                emailAddress);
+    public X500Name toX500Name() {
+        X500NameBuilder builder = new X500NameBuilder();
+        builder
+                .addRDN(BCStyle.COUNTRY_OF_RESIDENCE, countryName)
+                .addRDN(BCStyle.CN, commonName)
+                .addRDN(BCStyle.GIVENNAME, givenName)
+                .addRDN(BCStyle.SURNAME, surname)
+                .addRDN(BCStyle.EmailAddress, emailAddress)
+                .addRDN(BCStyle.L, localityName)
+                .addRDN(BCStyle.O, organizationName)
+                .addRDN(BCStyle.OU, organizationalUnitName)
+                .addRDN(BCStyle.ST, stateName);
+        return builder.build();
     }
 
     public String getCountryName() {
